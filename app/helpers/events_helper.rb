27e71eba9cc0869e6344735c22b200af97637e71
kazuma <wanwan@super.com>
2020-03-28 01:15:00 +0900

    過去のイベントを別ページに移動

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,8 @@
+require 'active_support/time' 
+
 module EventsHelper
+    #指定されたイベントが終了したか?
+    def event_finished?(event)
+        Time.current > event.end_time
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root to: 'pages#index'
   get 'pages/index'
   get 'pages/show'
+  get '/pages/show_old'
   resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
