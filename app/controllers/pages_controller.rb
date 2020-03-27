a09cdc9a59e5a1ead17e7be5ebecbaea0b242482
kazuma <wanwan@super.com>
2020-03-27 23:23:01 +0900

    ルートへのGETメソッドを受け取ったときにログインの有無によってリダイレクト先を変更

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
   before_action :sign_in_required, only: [:show]
   def index
+    if user_signed_in?
+      redirect_to '/pages/show'
+    else
+      redirect_to '/users/sign_in'
+    end
   end
 
   def show
