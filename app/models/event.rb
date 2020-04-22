a9946efc5210c3cc366b35fb3bc719f852ed4f68
kazuma <wanwan@super.com>
2020-04-22 13:11:57 +0900

    バリデーションの追加：イベント登録時に開始時間は終了時間より前の日時に指定する必要がある

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,7 @@ has_many :participants, class_name:  "Participant",
                         foreign_key: "event_id",
                         dependent:   :destroy
 has_many :users, through: :participants, source: :events
-validate :date_cannot_be_in_past
+validate :date_cannot_be_in_past, :start_date_cannot_be_bigger_than_end_date
 
   #参加者を追加する
   def self.add_user(id, user_id)
@@ -24,9 +24,14 @@ validate :date_cannot_be_in_past
   private 
     #バリデーション、過去の日程は指定できない
     def date_cannot_be_in_past
-      require 'byebug'; byebug
       if self.start_time < Date.today || self.end_time < Date.today
         errors.add(:past_date, "過去の日付は指定できません")
+      end
+    end
+
+    def start_date_cannot_be_bigger_than_end_date
+      if self.start_time > self.end_time
+        errors.add(:date_validation, "開始日時は終了日時より前に設定する必要があります")
       end
     end
 
