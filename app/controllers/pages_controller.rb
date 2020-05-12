2f3e68ceae284b1919a51b5ac9d0e8c0927f65a5
kazuma <wanwan@super.com>
2020-05-12 22:07:40 +0900

    [add] 今月の予定の開始日をカレンダー上で赤字表示する

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,9 +9,22 @@ class PagesController < ApplicationController
   end
 
   def show
+    # 今月のイベントで自分が主催者であるものを取得するクエリ カレンダー表示用
+    # !! 自分が参加者となっているが主催者でないものは取得できないので要修正
+    @monthly_events = Event.where("start_time LIKE ?",  "#{Date.today.year}-#{prefix(Date.today.month)}%")
+                           .where("user_id = ?", "#{current_user.id}")
   end
 
   def show_old
   end
 
+  private
+    # 0～9ならば先頭に文字0を付加するメソッド
+    def prefix(num)
+      if (num >= 0) && (num <= 9)
+        "0" + num.to_s
+      else
+        num.to_s
+      end
+    end
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,15 @@
 module PagesHelper
+
+    # 指定された日にイベントが予定されているかを返すメソッド
+    # !!ヘルパメソッドは同じ入力に対して同じ出力を返すべきとの観点からこのロジックは要検討
+    def has_event?(day)
+        for event in @monthly_events do
+            if event.start_time.day == day
+                return true
+            end
+        end
+        return false
+    end
 end
+
+
