d2768b3e02891e0815d48bb90eec446d65508d33
kazuma <wanwan@super.com>
2020-05-13 19:08:34 +0900

    [fix] 本番環境のメールドメイン名変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,12 +69,12 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'yoteiapp' }
+  config.action_mailer.default_url_options = { host: 'yotei_app.herokuapp.com' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
       user_name: ENV['SENDGRID_USERNAME'],
       password: ENV['SENDGRID_PASSWORD'],
-      domain: 'yoteiapp',
+      domain: 'heroku.com',
       address: 'smtp.sendgrid.net',
       port: 587,
       authentication: 'login',
