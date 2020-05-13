da299859a9aa29b84026517246878518cf51db21
kazuma <wanwan@super.com>
2020-05-13 16:50:42 +0900

    [add] カレンダーのマウスオーバー表示内容にイベント名と開始時間を追加

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,7 +1,9 @@
 module PagesHelper
 
     # 指定された日にイベントが予定されているかを返すメソッド
-    # !!ヘルパメソッドは同じ入力に対して同じ出力を返すべきとの観点からこのロジックは要検討
+    # [In] day: 日付(Integer)
+    # [Out] event_titles: 予定があるか(Boolean)
+    # !!ヘルパメソッドは同じ入力に対して同じ出力を返すべき観点からこのロジックは要検討
     def has_event?(day)
         for event in @monthly_events do
             if event.start_time.day == day
@@ -9,6 +11,20 @@ module PagesHelper
             end
         end
         return false
+    end
+
+    # 指定された日のイベント名を返却するメソッド
+    # [In] day: 日付(Integer)
+    # [Out] event_titles: イベント名(Str[])
+    # !!ヘルパメソッドは同じ入力に対して同じ出力を返すべき観点からこのロジックは要検討
+    def events_at_day(day)
+        event_titles = []
+        for event in @monthly_events do
+            if event.start_time.day == day
+                event_titles.push({:name => event.name, :time => event.start_time.strftime("%H:%M")})
+            end
+        end
+        event_titles
     end
 end
 
