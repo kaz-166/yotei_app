05df0504456c763893e52af0a2c2bb8a528e421d
kazuma <wanwan@super.com>
2020-05-13 16:59:32 +0900

    [refact] コメントの更新

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,8 +9,8 @@ class PagesController < ApplicationController
   end
 
   def show
-    # 今月のイベントで自分が主催者であるもの
-    # および自分が参加者であるものを取得するクエリ
+    # (今月中のイベント) AND ((自分が主催者であるもの) OR (自分が参加者であるもの))
+    # を取得するクエリ
     @monthly_events = Event.eager_load(:participants) #user_idをキーとして左外部結合
                            .where("events.start_time LIKE ?",  "#{Date.today.year}-#{prefix(Date.today.month)}%")
                            .where("events.user_id = ? OR participants.user_id = ?", "#{current_user.id}", "#{current_user.id}")
@@ -21,6 +21,8 @@ class PagesController < ApplicationController
 
   private
     # 0～9ならば先頭に文字0を付加するメソッド
+    # [In] 0～12の数字(Integer)
+    # [Out] プレフィクスが付加された文字列(Str)
     def prefix(num)
       if (num >= 0) && (num <= 9)
         "0" + num.to_s
