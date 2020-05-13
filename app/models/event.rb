8b29e5f2710c3c62e725676b65ba7e1bf6fc7131
kazuma <wanwan@super.com>
2020-05-13 17:49:01 +0900

    [fix] EventとPostのリレーションを修正

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@ class Event < ApplicationRecord
 #イベント参加者のデータベース関連
 has_many :participants
 has_many :users, through: :participants
+has_many :posts, dependent: :destroy
 
 validate :date_cannot_be_in_past, :start_date_cannot_be_bigger_than_end_date
 
