69987808c47454a76cae5c49456302ef1c6f1abc
kazuma <wanwan@super.com>
2020-05-14 10:43:43 +0900

    [add] prefixメソッドのテストを追加

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe PagesController, type: :controller do
+
+    describe "プライベートメソッドprefixのテスト" do
+        context "引数が一桁" do
+            it "should be prefixed" do
+                expect(PagesController.new.send(:prefix, 1)).to eq "01"
+                expect(PagesController.new.send(:prefix, 2)).to eq "02"
+                expect(PagesController.new.send(:prefix, 3)).to eq "03"
+                expect(PagesController.new.send(:prefix, 4)).to eq "04"
+                expect(PagesController.new.send(:prefix, 5)).to eq "05"
+                expect(PagesController.new.send(:prefix, 6)).to eq "06"
+                expect(PagesController.new.send(:prefix, 7)).to eq "07"
+                expect(PagesController.new.send(:prefix, 8)).to eq "08"
+                expect(PagesController.new.send(:prefix, 9)).to eq "09"
+            end
+        end
+
+        context "引数が二桁" do
+            it "should NOT be prefixed" do
+                expect(PagesController.new.send(:prefix, 10)).to eq "10"
+                expect(PagesController.new.send(:prefix, 11)).to eq "11"
+                expect(PagesController.new.send(:prefix, 12)).to eq "12"
+            end
+        end
+    end
+
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-
-   # 名がなければ無効な状態であること
-    it "is invalid without a username" do
-      user = User.new(username: nil)
-      user.valid?
-      expect(user.errors[:username]).to_not include("can't be blank")
-    end
-   # メールアドレスがなければ無効な状態であること
-   it "is invalid without an email address"
-   # 重複したメールアドレスなら無効な状態であること
-   it "is invalid with a duplicate email address"
-   # ユーザーのフルネームを文字列として返すこと
-   it "returns a user's full name as a string" 
 
 end
