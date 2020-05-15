94b5d6083aba29f0486664a8545168e8c99e10e3
kazuma <wanwan@super.com>
2020-05-15 11:29:15 +0900

    [add] コメントの文字数制限を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,14 @@
 class PostsController < ApplicationController
     
     def create
-      Post.create(content: post_params[:content], event_id: post_params[:event_id], user_id: current_user.id)
-      redirect_to event_path(post_params[:event_id])
+      @post = Post.new(content: post_params[:content], event_id: post_params[:event_id], user_id: current_user.id)
+      if @post.save
+        redirect_to event_path(post_params[:event_id])
+      else
+        @post = Post.where(event_id: post_params[:event_id])
+        @event = Event.find(post_params[:event_id])
+        render 'events/show'
+      end
     end
 
     def destroy
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
     belongs_to :event
+    # コメント内容は1文字以上200文字以下
+    validates :content, presence: true
+    validates :content, length: {minimum: Settings.post.content.min_length}
+    validates :content, length: {maximum: Settings.post.content.max_length}
 end
