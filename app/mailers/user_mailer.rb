b000acc25cade3a4e338337763d2854c4f1020b6
kazuma <wanwan@super.com>
2020-05-30 10:05:55 +0900

    [add] 予定をメールで送信

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,15 +2,15 @@ include EventsHelper
 class UserMailer < ApplicationMailer
 
     def remind(user)
-        @user = User.first
-        if has_recent_events?(@user.id).empty?
-        else
-          @text_html = "おはようございます。"+ @user.username + "さん。本日は以下の予定があります。<br>"
-          @events.each do |e|
-            @text_html += "予定名：" + e.name + "     開始時間：" + e.start_time.to_s + "<br>"
-          end
-          mail to: "#{@user.email}", subject: "[Yotei-App] 本日の予定"
+      @user = user
+      if has_recent_events?(@user.id).empty?
+      else
+        @text_html = "おはようございます。"+ @user.username + "さん。本日は以下の予定があります。<br>"
+        @events.each do |e|
+          @text_html += "予定名：" + e.name + "     開始時間：" + e.start_time.to_s + "<br>"
         end
+        mail to: "#{@user.email}", subject: "[Yotei-App] 本日の予定"
+      end
     end 
 
     private
@@ -18,7 +18,7 @@ class UserMailer < ApplicationMailer
         date = Date.today
         @events = Event.eager_load(:participants)
              .where("events.user_id = ? OR participants.user_id = ?", user_id, user_id)
-             .where("events.start_time::text LIKE ?",  "#{date.year}-#{PagesHelper.prefix(date.month)}-#{PagesHelper.prefix(date.day)}%")
+             #.where("events.start_time::text LIKE ?",  "#{date.year}-#{PagesHelper.prefix(date.month)}-#{PagesHelper.prefix(date.day)}%")
       end
 
 end
--- lib/tasks/remind.rake
+++ lib/tasks/remind.rake
@@ -1,7 +1,8 @@
 namespace :remind do
     desc '挨拶するメールを発行する。'
     task :remind => :environment do
-        user = User.first  # テスト用に自分のアカウントのみにメール送信
-        UserMailer.remind(user).deliver
+        User.all.each do |user|
+            UserMailer.remind(user).deliver
+        end
     end
 end
