532e6a4d68c19d0155fed822f3541445e6e65d5e
kazuma <wanwan@super.com>
2020-06-05 13:28:48 +0900

    [fix] 期間が一日のみの予定の場合カレンダーに反映されなかった問題の修正

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -6,8 +6,8 @@ module PagesHelper
     # !!ヘルパメソッドは同じ入力に対して同じ出力を返すべき観点からこのロジックは要検討
     def has_event?(date)
         for event in @monthly_events do
-            if (event.start_time <= date) &&
-               (event.end_time >= date) 
+            if (event.start_time.to_date <= date.to_date) &&
+               (event.end_time.to_date >= date.to_date) 
                 return true
             end
         end
@@ -21,8 +21,8 @@ module PagesHelper
     def events_at_day(date)
         event_titles = []
         for event in @monthly_events do
-            if (event.start_time <= date) &&
-               (event.end_time >= date) 
+            if (event.start_time.to_date <= date.to_date) &&
+                (event.end_time.to_date >= date.to_date) 
                 event_titles.push({:name => event.name, :time => event.start_time.strftime("%H:%M")})
             end
         end
