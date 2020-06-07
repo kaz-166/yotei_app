cf26bda6d7b45d5c7cd76ec3b6450c55f8ef1580
kazuma <wanwan@super.com>
2020-06-07 21:20:13 +0900

    [add] イベント作成テストのシナリオを追加

--- spec/system/pages_spec.rb
+++ spec/system/pages_spec.rb
@@ -2,14 +2,37 @@ require 'rails_helper'
 
 RSpec.feature 'Pages', type: :system do
 
-    scenario "ログインしてユーザページに遷移する" do
+    def log_in
         @user = create(:user)
-        create(:recent_event)
         visit '/users/sign_in'
         fill_in 'user_email', with: @user.email
         fill_in 'user_password', with: @user.password
         click_button 'Log in'
+    end
+
+    scenario "ログインしてユーザページに遷移する" do
+        log_in
         expect(page).to have_content('RECENT EVENTS')
     end
 
+    scenario "新規予定を作成する" do
+        log_in
+        visit '/events/new'
+        fill_in 'event_name', with: "新規予定"
+        fill_in 'event_abstract', with: "概要説明"
+        fill_in 'event_location', with: "場所"
+        select "2021", from: 'event_start_time_1i'
+        select "March", from:  'event_start_time_2i'
+        select "7", from:  'event_start_time_3i'
+        select "12", from: 'event_start_time_4i'
+        select "20", from: 'event_start_time_5i'
+        select "2021", from: 'event_end_time_1i'
+        select "March", from:  'event_end_time_2i'
+        select "7", from:  'event_end_time_3i'
+        select "12", from: 'event_end_time_4i'
+        select "40", from: 'event_end_time_5i'
+        click_button 'commit'
+        expect(page).to have_content('新規予定')
+    end
+    
 end
